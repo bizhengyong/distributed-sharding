7101feb5969f4ed88bbd80f0c713b8138345be51
bizhengyong <bizhengyong_xy@163.com>
2021-02-24 21:24:26 +0800

    9-5-分库分表-数据增删改查

--- src/test/java/com/imooc/distributedsharding/DistributedShardingApplicationTests.java
+++ src/test/java/com/imooc/distributedsharding/DistributedShardingApplicationTests.java
@@ -1,15 +1,18 @@
 package com.imooc.distributedsharding;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
 import java.util.Random;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@Slf4j
 public class DistributedShardingApplicationTests {
 
 //    @Test
@@ -22,9 +25,28 @@ public class DistributedShardingApplicationTests {
     @Test
     public void insert(){
         OrderEntity orderEntity = new OrderEntity();
-        orderEntity.setOrderId(System.currentTimeMillis());
+//        orderEntity.setOrderId(System.currentTimeMillis());
         orderEntity.setUserId(new Random().nextInt(999));
         orderDao.save(orderEntity);
     }
 
+    @Test
+    public void findByOrderId(){
+        OrderEntity byOrderId = orderDao.findByOrderId(571081152908472320L);
+        log.info("byOrderId={}",byOrderId);
+    }
+
+    @Test
+    public void findByUserId(){
+        List<OrderEntity> byUserId = orderDao.findByUserId(394);
+        log.info("byUserId={}",byUserId);
+    }
+
+    @Test
+    public void update() {
+        OrderEntity byOrderId = orderDao.findByOrderId(571443121222037504L);
+        log.info("byOrderId={}", byOrderId);
+        byOrderId.setUserId(999);
+        orderDao.save(byOrderId);
+    }
 }
